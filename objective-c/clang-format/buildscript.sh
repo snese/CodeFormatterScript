368f9d04d98af623fb272d7ede51b844ee70668e
Hung-Che Lo <hclo@snese.net>
2016-05-03 17:37:18 +0800

    add file list action

--- objective-c/clang-format/buildscript.sh
+++ objective-c/clang-format/buildscript.sh
@@ -2,7 +2,7 @@
 #  Using clang-format to format the source code
 #
 
-BASE_DIR="${PROJECT_DIR}"
+BASE_DIR="${PROJECT_DIR}/${PROJECT_NAME}"
 
 # confirm clang-format
 if [ -e /usr/local/bin/clang-format ]; then
@@ -13,3 +13,6 @@ echo "";
 echo "## clang-format not installed";
 exit 1;
 fi
+
+# get all .h, .m and .mm files
+find "$BASE_DIR" -type f -name "*.[mh]*" -exec echo '"{}" ' \;
