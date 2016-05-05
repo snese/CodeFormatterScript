bdd4da3496322150a242d33f00a4e3155e2a9adb
Hung-Che Lo <hclo@snese.net>
2016-05-05 08:50:47 +0800

    check the local and system clang-format

--- objective-c/clang-format/buildscript.sh
+++ objective-c/clang-format/buildscript.sh
@@ -3,16 +3,24 @@
 #
 
 BASE_DIR="${PROJECT_DIR}/${PROJECT_NAME}"
+# copy the execution file to the project utility folder
+UTIL_DIR="${PROJECT_DIR}/utility"
 
 # confirm clang-format
-if [ -e /usr/local/bin/clang-format ]; then
+if [ -e "$UTIL_DIR/clang-format" ]; then
 echo "";
-echo "## clang-format exist";
+echo "## local clang-format exist";
+CMD_PATH="$UTIL_DIR/clang-format"
+elif [ -e /usr/local/bin/clang-format ]; then
+echo "";
+echo "## sys clang-format exist";
+CMD_PATH="/usr/local/bin/clang-format"
 else
 echo "";
 echo "## clang-format not installed";
 exit 1;
 fi
 
+
 # get all .h, .m and .mm files and parse code files
-find "$BASE_DIR" -type f -name "*.[mh]*" -exec echo '"{}" ' \; | xargs clang-format -i -style=file
+find "$BASE_DIR" -type f -name "*.[mh]*" -exec echo '"{}" ' \; | xargs "$CMD_PATH" -i -style=file
